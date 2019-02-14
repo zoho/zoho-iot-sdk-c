f5908c6236d3291692b596a775d4471650fcdccf
Shahul Hameed. S <shahul.s@zohocorp.com>
2019-02-14 15:30:47 +0530

    fixed unit tests failures are not marking build state as failed.

--- scripts/clean_build_test.sh
+++ scripts/clean_build_test.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+set -e
+
 pwd
-rm -rf build && mkdir build && cd build && cmake .. && make -j8 && ctest -V
+
+rm -rf build
+mkdir build && cd build
+cmake ..
+make -j8
+echo "\n\n\nRunning Tests:\n"
+ctest -V
 echo "Done!"
