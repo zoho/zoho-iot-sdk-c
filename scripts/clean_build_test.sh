faf2be49a4d5cda448d93f166b8dcf4a9a8ec75d
Shahul Hameed. S <shahul.s@zohocorp.com>
2019-02-14 19:52:45 +0530

    updated build script to generate unit test report

--- scripts/clean_build_test.sh
+++ scripts/clean_build_test.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-set -e
-
 pwd
+set -e
 
 rm -rf build
 mkdir build && cd build
 cmake ..
 make -j8
+
 echo "\n\n\nRunning Tests:\n"
+export CMOCKA_MESSAGE_OUTPUT=XML
+export CMOCKA_XML_FILE=unit-test-report.xml
 ctest -V
+
 echo "\nSuccess! Generating Report:\n"
 lcov -c -d CMakeFiles/zoho_iot_sdk.dir/src/ -o coverage.info
 genhtml coverage.info -o report
