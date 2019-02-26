b6109e0339a4fa537ab0b6f77c973ba62e72ea84
Shahul Hameed. S <shahul.s@zohocorp.com>
2019-02-26 19:41:43 +0530

    updaetd build script to include Cobertura report

--- scripts/clean_build_test.sh
+++ scripts/clean_build_test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 pwd
+rm -rf build
+
 set -e
 
-rm -rf build
 mkdir build && cd build
 cmake ..
 make -j8
@@ -12,6 +13,11 @@ export CMOCKA_MESSAGE_OUTPUT=XML
 export CMOCKA_XML_FILE=unit-test-report.xml
 ctest -V
 
-echo "\nSuccess! Generating Report:\n"
+echo "\nSuccess! Generating Reports:\n"
 lcov -c -d CMakeFiles/zoho_iot_sdk.dir/src/ -o coverage.info
 genhtml coverage.info -o report
+
+#Cobertura Coverage Report
+gcovr -x -r ../ -e ../lib -e ../samples -e ../test -o coverage.xml
+
+cd -
--- scripts/make_test.sh
+++ scripts/make_test.sh
@@ -1,3 +1,7 @@
-pwd
-cd build && make -j8 && ctest -V && lcov -c -d CMakeFiles/zoho_iot_sdk.dir/src/ -o coverage.info && genhtml coverage.info -o report
+set -e
+cd build
+make -j8
+ctest -V
+#lcov -c -d CMakeFiles/zoho_iot_sdk.dir/src/ -o coverage.info && genhtml coverage.info -o report
+cd -
 echo "Done!"
