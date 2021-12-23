23328f9f90bceeb911fdafbab2ebdde46f852061
Shahul Hameed. S <shahul.s@zohocorp.com>
2021-12-23 08:48:52 +0530

    support to get dynamic build time parameters through docker.

--- cross-compile/OpenWRT/toolchain.cmake
+++ cross-compile/OpenWRT/toolchain.cmake
@@ -1,17 +1,22 @@
+SET(ENV{STAGING_DIR} "/tool_chain/staging_dir/")
 INCLUDE(CMakeForceCompiler)
 
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_VERSION 1)
 
-# The toolchain rootpart for the Cloudgate device from Option which implements OpenWRT is as below.
+# The toolchain for the Cloudgate device from Option which implements OpenWRT is as below.
 # For other OpenWRT based devices, edit the toolchain root path and the compiler correspondingly.
 
 # Update your toolchain root path below:
-SET(CMAKE_FIND_ROOT_PATH /Path_to_SDK/cloudgate-sdk-x86_64-{version}/staging_dir/toolchain-arm_v5te_gcc-4.4.3_uClibc-0.9.33.2_eabi/)
+SET(CMAKE_STAGING_DIR /tool_chain/staging_dir/)
+SET(CMAKE_FIND_ROOT_PATH ${CMAKE_STAGING_DIR}toolchain-arm_v5te_gcc-4.4.3_uClibc-0.9.33.2_eabi/)
 
 # Update your compiler name below:
 SET(TOOLCHAIN_COMPILER /bin/arm-openwrt-linux-gcc)
-SET(ENV{STAGING_DIR} ${CMAKE_FIND_ROOT_PATH})
+
+#Seeting STAGING_DIR ENV through Cmake is not working, so doing it through shell
+#SET(ENV{STAGING_DIR} ${CMAKE_STAGING_DIR})
+#message("Environment variable : $ENV{STAGING_DIR}")
 
 SET(CMAKE_C_COMPILER ${CMAKE_FIND_ROOT_PATH}${TOOLCHAIN_COMPILER})
 
