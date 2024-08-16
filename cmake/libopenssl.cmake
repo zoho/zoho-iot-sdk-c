d4de313d46024ac83ec2f00b9b4d5080c2fa6f33
sabaresh-17844 <sabaresh.bv@zohocorp.com>
2024-08-16 16:25:21 +0530

    updated openssl library

--- cmake/libopenssl.cmake
+++ cmake/libopenssl.cmake
@@ -1,6 +1,6 @@
 include(ExternalProject)
-set(OPENSSL_LIBRARY_VERSION 3.1.3)  # Replace with the desired Paho version
-set(OPENSSL_DIR ${CMAKE_CURRENT_SOURCE_DIR}/lib/openssl-3.1.3)
+set(OPENSSL_LIBRARY_VERSION 3.3.1)  # Replace with the desired Paho version
+set(OPENSSL_DIR ${CMAKE_CURRENT_SOURCE_DIR}/lib/openssl-3.3.1)
 set(OPENSSL_BIN ${CMAKE_CURRENT_BINARY_DIR}/libopenssl)
 set(OPENSSL_STATIC_LIB ${OPENSSL_BIN}/lib/libssl.a)
 set(OPENSSL_STATIC_CRYPTO_LIB ${OPENSSL_BIN}/lib/libcrypto.a)
@@ -14,7 +14,7 @@ ExternalProject_Add(
   libopenssl
   PREFIX ${OPENSSL_BIN}
   SOURCE_DIR ${OPENSSL_DIR}
-  URL https://github.com/openssl/openssl/releases/download/openssl-${OPENSSL_LIBRARY_VERSION}/openssl-3.1.3.tar.gz
+  URL https://github.com/openssl/openssl/releases/download/openssl-${OPENSSL_LIBRARY_VERSION}/openssl-${OPENSSL_LIBRARY_VERSION}.tar.gz
   CONFIGURE_COMMAND ${OPENSSL_DIR}/Configure ${OS_NAME} --prefix=${OPENSSL_BIN} -static -fPIC CC=${CMAKE_C_COMPILER} ${ADDITIONAL_CONFIG_FLAG} 
   BUILD_COMMAND make -j4 VERBOSE=1
   BUILD_BYPRODUCTS ${OPENSSL_STATIC_LIB} ${OPENSSL_STATIC_CRYPTO_LIB}
@@ -22,8 +22,8 @@ ExternalProject_Add(
 
 # Check for lib64
 if (NOT EXISTS ${OPENSSL_STATIC_LIB})
-    set(OPENSSL_STATIC_LIB ${OPENSSL_BIN}/lib/libssl.a)
-    set(OPENSSL_STATIC_CRYPTO_LIB ${OPENSSL_BIN}/lib/libcrypto.a)
+    set(OPENSSL_STATIC_LIB ${OPENSSL_BIN}/lib64/libssl.a)
+    set(OPENSSL_STATIC_CRYPTO_LIB ${OPENSSL_BIN}/lib64/libcrypto.a)
 endif()
 
 add_library(openssl_ssl STATIC IMPORTED GLOBAL)
