0c0e5b92c60dc54575a57df2a6876d20248833fd
Renga <srirengarajan.pb@zohocorp.com>
2025-04-28 17:38:01 +0530

    openssl fix

--- cmake/libpaho_c.cmake
+++ cmake/libpaho_c.cmake
@@ -3,25 +3,45 @@ set(PAHO_LIBRARY_VERSION 1.3.13)
 set(PAHO_DIR ${PROJECT_SOURCE_DIR}/lib/paho.mqtt.c)
 set(PAHO_BIN ${CMAKE_CURRENT_BINARY_DIR}/libpaho)
 IF(Z_ENABLE_TLS)
-set(PAHO_STATIC_LIB ${PAHO_BIN}/lib/libpaho-mqtt3cs.a)
-set(PAHO_C_TLS TRUE)
+    set(PAHO_STATIC_LIB ${PAHO_BIN}/lib/libpaho-mqtt3cs.a)
+    set(PAHO_C_TLS TRUE)
 ELSE()
-set(PAHO_STATIC_LIB ${PAHO_BIN}/lib/libpaho-mqtt3c.a)
-set(PAHO_C_TLS FALSE)
+    set(PAHO_STATIC_LIB ${PAHO_BIN}/lib/libpaho-mqtt3c.a)
+    set(PAHO_C_TLS FALSE)
 ENDIF(Z_ENABLE_TLS)
 set(PAHO_INCLUDES ${PAHO_BIN}/include)
 
-IF(Z_STATIC_OPENSSL)
-  set(OPENSSL_BIN ${OPENSSL_BIN})
-  set(OPENSSL_INCLUDE_DIR ${OPENSSL_INCLUDES})
-  set(OPENSSL_SSL_LIBRARY ${OPENSSL_STATIC_LIB})
-  set(OPENSSL_CRYPTO_LIBRARY ${OPENSSL_STATIC_CRYPTO_LIB})
-  set(PAHO_STATIC_LIB ${PAHO_BIN}/lib/libpaho-mqtt3cs.a)
-  set(PAHO_C_TLS TRUE)
-ENDIF(Z_STATIC_OPENSSL)
+set(CMAKE_ARGS
+    -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+    -DCMAKE_INSTALL_LIBDIR=lib
+    -DPAHO_HIGH_PERFORMANCE=TRUE
+    -DPAHO_BUILD_STATIC=TRUE
+    -DPAHO_WITH_SSL=${PAHO_C_TLS}
+    -DPAHO_HIGH_PERFORMANCE=TRUE
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+)
+
+IF(Z_ENABLE_TLS)
+    IF(Z_STATIC_OPENSSL)
+        set(OPENSSL_BIN ${OPENSSL_BIN})
+        set(OPENSSL_INCLUDE_DIR ${OPENSSL_INCLUDES})
+        set(OPENSSL_SSL_LIBRARY ${OPENSSL_STATIC_LIB})
+        set(OPENSSL_CRYPTO_LIBRARY ${OPENSSL_STATIC_CRYPTO_LIB})
+        set(PAHO_STATIC_LIB ${PAHO_BIN}/lib/libpaho-mqtt3cs.a)
+        list(APPEND CMAKE_ARGS 
+            -DOPENSSL_ROOT_DIR=${OPENSSL_BIN}
+            -DOPENSSL_CRYPTO_LIBRARY=${OPENSSL_CRYPTO_LIBRARY}
+            -DOPENSSL_INCLUDE_DIR=${OPENSSL_INCLUDE_DIR}
+            -DOPENSSL_SSL_LIBRARY=${OPENSSL_SSL_LIBRARY}
+        )
+    ELSE()
+        find_package(OpenSSL REQUIRED)
+        message(STATUS "OpenSSL libraries: ${OPENSSL_LIBRARIES}")
+    ENDIF(Z_STATIC_OPENSSL)
+ENDIF(Z_ENABLE_TLS)
 
 file(MAKE_DIRECTORY ${PAHO_INCLUDES})
-message(STATUS "OPENSSL_BIN: ${OPENSSL_BIN}")
+
 
 IF(NOT EXISTS ${PAHO_DIR})
     MESSAGE("\nDownloading paho c\n")
@@ -52,18 +72,7 @@ ExternalProject_Add(
   PREFIX ${PAHO_BIN}
   SOURCE_DIR ${PAHO_DIR}
   #URL https://github.com/eclipse/paho.mqtt.c/archive/v${PAHO_LIBRARY_VERSION}.tar.gz
-  CMAKE_ARGS 
-          -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> 
-          -DCMAKE_INSTALL_LIBDIR=lib 
-          -DPAHO_HIGH_PERFORMANCE=TRUE 
-          -DPAHO_BUILD_STATIC=TRUE 
-          -DPAHO_WITH_SSL=${PAHO_C_TLS} 
-          -DPAHO_HIGH_PERFORMANCE=TRUE 
-          -DOPENSSL_ROOT_DIR=${OPENSSL_BIN}
-          -DOPENSSL_CRYPTO_LIBRARY=${OPENSSL_CRYPTO_LIBRARY}
-          -DOPENSSL_INCLUDE_DIR=${OPENSSL_INCLUDE_DIR}
-          -DOPENSSL_SSL_LIBRARY=${OPENSSL_SSL_LIBRARY}
-          -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}
+  CMAKE_ARGS ${CMAKE_ARGS}
 )
 
 add_library(paho STATIC IMPORTED GLOBAL)
@@ -74,13 +83,17 @@ ENDIF(Z_STATIC_OPENSSL)
 set_target_properties(paho PROPERTIES IMPORTED_LOCATION ${PAHO_STATIC_LIB})
 set_target_properties(paho PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${PAHO_INCLUDES})
 
-execute_process(
-    COMMAND ${CMAKE_COMMAND} -E create_symlink
-    ${OPENSSL_BIN}/libssl.so.1.1 ${OPENSSL_BIN}/libssl.so
-)
-execute_process(
-    COMMAND ${CMAKE_COMMAND} -E create_symlink
-    ${OPENSSL_BIN}/libcrypto.so.1.1 ${OPENSSL_BIN}/libcrypto.so
-)
+IF(Z_ENABLE_TLS)
+    IF(Z_STATIC_OPENSSL)
+        execute_process(
+            COMMAND ${CMAKE_COMMAND} -E create_symlink
+            ${OPENSSL_BIN}/libssl.so.1.1 ${OPENSSL_BIN}/libssl.so
+        )
+        execute_process(
+            COMMAND ${CMAKE_COMMAND} -E create_symlink
+            ${OPENSSL_BIN}/libcrypto.so.1.1 ${OPENSSL_BIN}/libcrypto.so
+        )
 
-link_directories(${OPENSSL_BIN})
+        link_directories(${OPENSSL_BIN})
+    ENDIF(Z_STATIC_OPENSSL)
+ENDIF(Z_ENABLE_TLS)   
